f4d304c4705b318297844188b4387fb35f5f7db9
João Pedro Gava Ribeiro <jpribeirogava@gmail.com>
2026-04-05 17:49:07 -0300

    Update ops.py

--- ops.py
+++ ops.py
@@ -57,7 +57,7 @@ def consultar(patente):
     if not token: token = get_token()
     
     # URL de serviços continua com /rest-services/
-    url = f"https://ops.epo.org/rest-services/published-data/family/publication/epodoc/{patente}"
+    url = f"https://ops.epo.org/rest-services/published-data/family/application/epodoc/{patente}"
     
     res = requests.get(url, headers={"Authorization": f"Bearer {token}", "Accept": "application/json"})
     
